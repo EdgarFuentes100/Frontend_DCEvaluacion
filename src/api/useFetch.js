bca05f74680e639aa97792d876cdee79e49a579a
EdgarFuentes100 <edgarfuentes139@gmail.com>
2026-01-29 20:45:02 -0800

    produccion

--- src/api/useFetch.js
+++ src/api/useFetch.js
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 
 function useFetch() {
-  const urlBase = process.env.REACT_APP_API_BASE_URL || "http://localhost:4000/api/v1/";
+  const urlBase = "https://backend-dcevaluacion.onrender.com/api/v1" || "http://localhost:4000/api/v1/";
 
   const manejarRespuesta = async (response) => {
     const contentType = response.headers.get("content-type") || "";
